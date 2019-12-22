35a13da776ad6cd67046b2c708111effb92e7f1d
Istinra <forsaken.panda@gmail.com>
2019-12-22 21:26:01 +1100

    canvas test

--- client/src/index.ts
+++ client/src/index.ts
@@ -1,11 +1,44 @@
-export function performRequestAndLogResult() {
-    console.log("we woo wee woo");
-    const req = new XMLHttpRequest();
-    req.open("GET", "/request", true);
-    req.onload = () => {
-        console.log(req.responseText);
-    };
-    req.send();
+import {State} from "./state";
+
+const canvas = document.getElementById("renderTarget") as HTMLCanvasElement;
+const ctx = canvas.getContext("2d") as CanvasRenderingContext2D;
+
+const state: State = {
+    playerPos: {
+        x: 0,
+        y: 0
+    }
+};
+
+function drawBg() {
+    ctx.fillStyle = "black";
+    ctx.fillRect(0, 0, 600, 600);
 }
 
-document.getElementById("theButton")?.addEventListener("click", performRequestAndLogResult);
+function drawPlayer() {
+    ctx.fillStyle = "white";
+    ctx.fillRect(state.playerPos.x, state.playerPos.y, 10, 10);
+}
+
+function frame(dt: number) {
+    drawBg();
+    drawPlayer();
+    window.requestAnimationFrame(frame);
+}
+
+window.requestAnimationFrame(frame);
+
+document.addEventListener("keydown", (event: KeyboardEvent) => {
+    if (event.key === "a") {
+        state.playerPos.x -= 10;
+    }
+    if (event.key === "d") {
+        state.playerPos.x += 10;
+    }
+    if (event.key === "w") {
+        state.playerPos.y -= 10;
+    }
+    if (event.key === "s") {
+        state.playerPos.y += 10;
+    }
+});
