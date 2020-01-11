02e4037f5a34ddd47a40943fb7f660d0f7be63e9
Istinra <forsaken.panda@gmail.com>
2020-01-11 12:44:54 +1100

    Map rendering base64 data

--- client/src/renderer.ts
+++ client/src/renderer.ts
@@ -1,21 +1,21 @@
 import {Player} from "./entity";
-import world from "./world.json";
 import {MapArea} from "./game";
+
+let world = require("./world.json");
 
 export class Renderer {
 
-    private maps: { [key: number]: MapArea};
+    private maps: { [key: number]: MapArea };
 
 
     constructor(private ctx: CanvasRenderingContext2D) {
-        const a: any = world;
-        this.maps = JSON.parse(world);
+        this.maps = world
     }
 
     public drawBackground(): void {
 
         let image = new Image();
-        // image.src = Pallet;
+        image.src = "data:image/png;base64," + this.maps[0].imageData;
         this.ctx.fillStyle = "black";
         this.ctx.fillRect(0, 0, 600, 600);
         this.ctx.drawImage(image, 0, 0);
